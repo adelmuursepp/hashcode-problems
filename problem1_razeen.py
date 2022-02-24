8501ddc9bc04f345e32c0db4ec6e16d808ad30cb
Razeen <razeen_ali@yahoo.com>
2022-02-24 13:34:55 -0500

    project read

--- problem1_razeen.py
+++ problem1_razeen.py
@@ -1,9 +1,4 @@
-import os
-
-# code which gets the absolute path of the input_data directory
-dir_path = os.path.dirname(os.path.realpath(__file__))
-input_data_path = os.path.join(dir_path, 'input_data')
-f = open(f'{input_data_path}\\a_an_example.in.txt', "r")
+f = open("input_data/a_an_example.in.txt", "r")
 
 user_dict = {}
 
@@ -12,3 +7,38 @@ user_dict = {}
 first_line = f.readline().split(" ")
 contributors = int(first_line[0])
 projects = int(first_line[1])
+
+for contributor in range(contributors):
+    user, skills = f.readline().split(" ")
+    skills = int(skills)
+    user_dict[user] = {}
+    for skill in range(skills):
+        skill_name, skill_level = f.readline().split()
+        skill_level = int(skill_level)
+        user_dict[user][skill_name] = skill_level
+
+
+project_dict = {}
+
+for project in range(projects):
+    project_name, days_required, score_awarded, best_before, num_roles = f.readline().split(" ")
+    days_required = int(days_required)
+    score_awarded = int(score_awarded)
+    best_before = int(best_before)
+    num_roles = int(num_roles)
+    project_dict[project_name] = {}
+    project_dict[project_name]["days_required"] = days_required
+    project_dict[project_name]["score_awarded"] = score_awarded
+    project_dict[project_name]["best_before"] = best_before
+    project_dict[project_name]["num_roles"] = num_roles
+    project_dict[project_name]["roles"] = {}
+
+    for role in range(num_roles):
+        role_name, required_skills = f.readline().split(" ")
+        required_skills = int(required_skills)
+        project_dict[project_name]["roles"][role_name] = required_skills
+  
+        
+    
+print(project_dict)
+print(user_dict)
