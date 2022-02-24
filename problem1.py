a9d69c6ef89767033faa3cc8259a3bc66ac0b34b
Adel Muursepp <adelmuursepp@utmuser167-173.wireless.utoronto.ca>
2022-02-24 15:12:14 -0500

    created input for razeen

--- problem1.py
+++ problem1.py
@@ -66,6 +66,31 @@ def relative_skill(user, project):
     return relative_skill_tracker
          
 
+i = 0
+for i in range(100):
+    for project in project_dict:
+        # print(project)
+        total_days = int(project_dict[project]["best_before"]) + i 
+        if total_days > int(project_dict[project]["best_before"]):
+            points = int(project_dict[project]["score_awarded"]) - (total_days - int(project_dict[project]["best_before"]))
+        users_available = []
+        for user in user_dict:
+            if is_day_available(user, i):
+                users_available.append(user)
+        skills = project_dict[project]["roles"]
+        users_with_rel_skills = {}
+        for user in users_available:
+            relative_score = relative_skill(user_dict[user], project_dict[project])
+            if relative_score == {}:
+                pass
+            else:
+                for relative_skill in relative_score:
+                    if relative_score[relative_skill] >= -1:
+                        users_with_rel_skills[user] = relative_score[relative_skill]
+                    else:
+                        pass
+
+                    
 
 print("Projects")
 print(project_dict)
